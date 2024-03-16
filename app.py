5a115962d4604a8d8ca7eee8c30c52b6e8e805a6
sapapesh <162401644+sapapesh@users.noreply.github.com>
2024-03-15 21:30:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import plotly.express as px
+from shiny import render
 from shiny.express import input, ui
 from shinywidgets import render_plotly
 import palmerpenguins  # This package provides the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
+import pandas as pd
+import seaborn as sns
+from shiny import reactive, render, req
 
 ui.page_opts(title="Sarah's Penguin Data", fillable=True)
 with ui.layout_columns():
     
     with ui.sidebar(open="open", bg="#f8f8f8"):  
         ui.h2("Sidebar")
+        
+        ui.input_selectize(  
+        "selectize",  
+        "Select an option below:",  
+        ["Adelie", "Gentoo", "Chinstrap"],
+    )
 
     @render_plotly
     def plot1():
